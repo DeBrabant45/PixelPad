eacee203245daf1f62b54f08fb545c039f90c3f4
Aaron <debrabant45@gmail.com>
2025-05-17 15:58:30 -0400

    Cleaning up SDLRenderer & removing Resize

--- Infrastructure/include/Renderers/SDLRenderer.hpp
+++ Infrastructure/include/Renderers/SDLRenderer.hpp
@@ -5,6 +5,7 @@
 
 struct SDL_Renderer;
 struct SDL_Window;
+struct SDL_Texture;
 
 namespace PixelPad::Core
 {
@@ -18,14 +19,21 @@ namespace PixelPad::Infrastructure
 	public:
 		SDLRenderer(IWindow& window);
 		~SDLRenderer() override;
+		void CreateRenderer();
+		void Shutdown() override;
 		void ClearScreen() override;
 		void Render(const PixelPad::Core::Canvas& canvas) override;
 		void Present() override;
-		void Resize(int newWidth, int newHeight) override;
-		void Shutdown() override;
+
+	private:
+		void CreateCanvasTexture(int width, int height);
+		bool UpdateTextures(const std::vector<int>& pixels, int width, int height);
 
 	private:
 		IWindow& m_window;
 		SDL_Renderer* m_sdlRenderer{ nullptr };
+		SDL_Texture* m_canvasTexture{ nullptr };
+		int m_canvasWidth{ 0 };
+		int m_canvasHeight{ 0 };
 	};
 }
