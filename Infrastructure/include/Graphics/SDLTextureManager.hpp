03fbf6709b7a3f3725329fb8c88165fd382b1da2
Aaron <debrabant45@gmail.com>
2025-10-10 14:57:36 -0400

    Refactor SDLTextureManager to load defaults & use path provider;

--- Infrastructure/include/Graphics/SDLTextureManager.hpp
+++ Infrastructure/include/Graphics/SDLTextureManager.hpp
@@ -3,18 +3,27 @@
 #include "Graphics/ITextureManager.hpp"
 #include "Graphics/ITexture.hpp"
 #include "Renderers/SDLRenderer.hpp"
+#include "Paths/IPathProvider.hpp"
+
 #include <unordered_map>
+#include <string>
 
 namespace PixelPad::Infrastructure
 {
 	class SDLTextureManager : public PixelPad::Application::ITextureManager
 	{
 	public:
-		SDLTextureManager(SDLRenderer& renderer);
+		SDLTextureManager(SDLRenderer& renderer, const IPathProvider& pathProvider);
 		std::shared_ptr<PixelPad::Application::ITexture> LoadTexture(const std::string& path) override;
+
+	private:
+		std::shared_ptr<PixelPad::Application::ITexture> LoadDefaultTexture();
 
 	private:
 		SDLRenderer& m_renderer;
 		std::unordered_map<std::string, std::weak_ptr<PixelPad::Application::ITexture>> m_cache;
+		std::shared_ptr<PixelPad::Application::ITexture> m_defaultTexture;
+		const IPathProvider& m_pathProvider;
+		const std::string m_missingTextureKey = "--missing--.bmp";
 	};
 }
