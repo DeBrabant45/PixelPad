8b55a9edc9ae38d42961ac4261070cda1898f71c
Aaron <debrabant45@gmail.com>
2025-11-08 15:28:07 -0500

    Update UIButton to send out an dynamic event;

--- Infrastructure/include/Graphics/UIButton.hpp
+++ Infrastructure/include/Graphics/UIButton.hpp
@@ -2,10 +2,15 @@
 
 #include "Graphics/IUIButton.hpp"
 #include "Graphics/ITexture.hpp"
+#include "Graphics/IUIRoot.hpp"
 #include "Buses/EventBus.hpp"
 #include "Geometries/Transform.hpp"
 #include "Graphics/IButtonSprite.hpp"
+#include "Renderers/IRenderer.hpp"
+#include "Events/UIButtonClickedEvent.hpp"
+#include <Events/MouseButtonEvent.hpp>
 
+#include <iostream>
 #include <memory>
 #include <string>
 
@@ -16,27 +21,66 @@ namespace PixelPad::Application
 
 namespace PixelPad::Infrastructure
 {
-	class UIButton : public PixelPad::Application::IUIButton
-	{
-	public:
-		UIButton(
-			std::string name, 
-			PixelPad::Core::Transform& transform, 
-			std::unique_ptr<PixelPad::Application::IButtonSprite> sprite, 
-			PixelPad::Infrastructure::EventBus& eventBus);
-		~UIButton() override;
-		void Render(PixelPad::Application::IRenderer& renderer) override;
-		void OnClick(int clickX, int clickY) override;
+    template<typename TClickEventValue>
+    class UIButton : public PixelPad::Application::IUIButton
+    {
+    public:
+        UIButton(
+            PixelPad::Core::Transform transform,
+            std::unique_ptr<PixelPad::Application::IButtonSprite> sprite,
+            PixelPad::Infrastructure::EventBus& eventBus,
+            TClickEventValue clickEventValue)
+            : m_transform(std::move(transform)),
+            m_sprite(std::move(sprite)),
+            m_eventBus(eventBus),
+            m_clickEventValue(clickEventValue)
+        {
+            RegisterEventHandlers();
+        }
 
-	private:
-		void RegisterEventHandlers();
-		void UnregisterEventHandlers();
+        ~UIButton() override { UnregisterEventHandlers(); }
 
-	private:
-		std::string m_name;
-		PixelPad::Core::Transform m_transform;
-		std::unique_ptr<PixelPad::Application::IButtonSprite> m_sprite;
-		PixelPad::Infrastructure::EventBus& m_eventBus;
-		PixelPad::Infrastructure::EventBus::SubscriptionToken m_mouseEventToken;
-	};
+        void Render(PixelPad::Application::IRenderer& renderer) override
+        {
+            if (!m_sprite->GetTexture().get())
+                return;
+
+            renderer.Render(m_sprite.get());
+        }
+
+        void OnClick(int clickX, int clickY) override
+        {
+            if (clickX >= m_transform.X && clickX <= m_transform.X + m_transform.Width &&
+                clickY >= m_transform.Y && clickY <= m_transform.Y + m_transform.Height)
+            {
+                m_eventBus.Publish(
+                    PixelPad::Application::UIButtonClickedEvent<TClickEventValue>(m_clickEventValue));
+            }
+        }
+
+    private:
+        void RegisterEventHandlers()
+        {
+            m_mouseEventToken = m_eventBus.Subscribe<PixelPad::Application::MouseButtonEvent>(
+                [this](const PixelPad::Application::MouseButtonEvent& evt)
+                {
+                    if (!evt.IsPressed)
+                        return;
+
+                    OnClick(evt.X, evt.Y);
+                });
+        }
+
+        void UnregisterEventHandlers()
+        {
+            m_eventBus.Unsubscribe<PixelPad::Application::MouseButtonEvent>(m_mouseEventToken);
+        }
+
+    private:
+        PixelPad::Core::Transform m_transform;
+        std::unique_ptr<PixelPad::Application::IButtonSprite> m_sprite;
+        PixelPad::Infrastructure::EventBus& m_eventBus;
+        TClickEventValue m_clickEventValue;
+        PixelPad::Infrastructure::EventBus::SubscriptionToken m_mouseEventToken;
+    };
 }
