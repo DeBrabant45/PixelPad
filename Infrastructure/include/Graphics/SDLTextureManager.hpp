dd369bf25c070da9ea3f267309f83c55ef97493e
Aaron <debrabant45@gmail.com>
2025-11-08 15:32:06 -0500

    Add Texture creation with Colors;

--- Infrastructure/include/Graphics/SDLTextureManager.hpp
+++ Infrastructure/include/Graphics/SDLTextureManager.hpp
@@ -15,13 +15,15 @@ namespace PixelPad::Infrastructure
 	public:
 		SDLTextureManager(SDLRenderer& renderer, const IPathProvider& pathProvider);
 		std::shared_ptr<PixelPad::Application::ITexture> LoadTexture(const std::string& path) override;
+		std::shared_ptr<PixelPad::Application::ITexture> LoadTexture(PixelPad::Core::Color color) override;
 
 	private:
 		std::shared_ptr<PixelPad::Application::ITexture> LoadDefaultTexture();
 
 	private:
 		SDLRenderer& m_renderer;
-		std::unordered_map<std::string, std::weak_ptr<PixelPad::Application::ITexture>> m_cache;
+		std::unordered_map<std::string, std::weak_ptr<PixelPad::Application::ITexture>> m_pathTextureCache;
+		std::unordered_map<uint32_t, std::weak_ptr<PixelPad::Application::ITexture>> m_ColorTextureCache;
 		std::shared_ptr<PixelPad::Application::ITexture> m_defaultTexture;
 		const IPathProvider& m_pathProvider;
 		const std::string m_missingTextureKey = "--missing--.bmp";
