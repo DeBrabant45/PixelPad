9f1219418ca5009741f067fb7a489cbbb773cc5b
Aaron <debrabant45@gmail.com>
2025-12-07 12:33:32 -0500

    Add ui button state to ui tools panel

--- Infrastructure/include/Graphics/UIToolsPanel.hpp
+++ Infrastructure/include/Graphics/UIToolsPanel.hpp
@@ -8,6 +8,7 @@
 
 #include <array>
 #include <memory>
+#include <cstdint>
 
 namespace PixelPad::Application
 {
@@ -20,13 +21,20 @@ namespace PixelPad::Infrastructure
 	{
 	public:
 		UIToolsPanel(PixelPad::Application::ISpriteFactory& spriteFactory, PixelPad::Infrastructure::EventBus& eventBus);
+		~UIToolsPanel();
 		void Render(PixelPad::Application::IRenderer& renderer) override;
 
 	private:
+		void RegisterEventHandlers();
+		void UnregisterEventHandlers();
 		void CreateToolButtons(PixelPad::Infrastructure::EventBus& eventBus);
+		void SetActiveButton(std::uint32_t id);
+		bool OwnsButton(std::uint32_t id);
 
 	private:
 		PixelPad::Application::ISpriteFactory& m_spriteFactory;
+		PixelPad::Infrastructure::EventBus& m_eventBus;
 		std::vector<std::unique_ptr<PixelPad::Infrastructure::UIButton<PixelPad::Core::ToolType>>> m_buttons;
+		PixelPad::Infrastructure::EventBus::SubscriptionToken m_buttonIdToken;
 	};
 }
