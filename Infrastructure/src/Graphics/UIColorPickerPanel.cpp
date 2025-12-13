d6064b1fcfdbb1165c73d73931ceab9e58f641b9
Aaron <debrabant45@gmail.com>
2025-12-13 15:18:34 -0500

    Add UIComponent for color display to UIColorPickerPanel

--- Infrastructure/src/Graphics/UIColorPickerPanel.cpp
+++ Infrastructure/src/Graphics/UIColorPickerPanel.cpp
@@ -1,18 +1,111 @@
 #include "Graphics/UIColorPickerPanel.hpp"
 #include "Graphics/Color.hpp"
+#include "Events/ColorChangedEvent.hpp"
+#include <Graphics/UIComponent.hpp>
 
 namespace PixelPad::Infrastructure
 {
 	UIColorPickerPanel::UIColorPickerPanel(PixelPad::Application::ISpriteFactory& spriteFactory, PixelPad::Infrastructure::EventBus& eventBus) :
 		m_spriteFactory(spriteFactory),
-		m_buttons()
+        m_eventBus(eventBus),
+		m_buttons(),
+        m_colorPreviewComponent(),
+        m_colorChangeToken()
 	{
-		CreateButtons(eventBus);
+        RegisterEventHandlers();
+        CreateColorPreviewComponent();
+		CreateUIButtons(eventBus);
 	}
 
-    void UIColorPickerPanel::CreateButtons(PixelPad::Infrastructure::EventBus& eventBus)
+    UIColorPickerPanel::~UIColorPickerPanel()
     {
-        std::vector<PixelPad::Core::Color> colors = {
+        UnregisterEventHandlers();
+    }
+
+    void UIColorPickerPanel::Render(PixelPad::Application::IRenderer& renderer)
+    {
+        m_colorPreviewComponent->Render(renderer);
+        for (auto& button : m_buttons)
+        {
+            button->Render(renderer);
+        }
+    }
+
+    void UIColorPickerPanel::RegisterEventHandlers()
+    {
+        m_colorChangeToken = m_eventBus.Subscribe<PixelPad::Application::UIButtonClickedEvent<PixelPad::Core::Color>>(
+            [this](const PixelPad::Application::UIButtonClickedEvent<PixelPad::Core::Color>& evt)
+            {
+                UpdateColorPreviewComponent(evt.Value);
+            });
+    }
+
+    void UIColorPickerPanel::UnregisterEventHandlers()
+    {
+        m_eventBus.Unsubscribe<
+            PixelPad::Application::UIButtonClickedEvent<PixelPad::Application::ClickResult>>(m_colorChangeToken);
+    }
+
+    void UIColorPickerPanel::CreateColorPreviewComponent()
+    {
+        auto defaultColor = GetColors()[0];
+        PixelPad::Core::Transform transform(8, 545, 0, 27, 28);
+        auto sprite = m_spriteFactory.CreateSprite(defaultColor, transform);
+        m_colorPreviewComponent = std::make_unique<PixelPad::Infrastructure::UIComponent>(
+            transform,
+            std::move(sprite)
+        );
+    }
+
+    void UIColorPickerPanel::UpdateColorPreviewComponent(PixelPad::Core::Color color)
+    {
+        if (!m_colorPreviewComponent)
+            return;
+
+        const auto& transform = m_colorPreviewComponent->GetTransform();
+        auto newSprite = m_spriteFactory.CreateSprite(color, transform);
+        m_colorPreviewComponent->SetSprite(std::move(newSprite));
+    }
+
+    void UIColorPickerPanel::CreateUIButtons(PixelPad::Infrastructure::EventBus& eventBus)
+    {
+        int startX = 40;
+        int startY = 545;
+        int size = 13;
+        int spacing = 3;
+        int rows = 2;
+        auto colors = GetColors();
+        int cols = (colors.size() + rows - 1) / rows;
+
+        for (size_t i = 0; i < colors.size(); ++i)
+        {
+            int row = i / cols;
+            int col = i % cols;
+
+            PixelPad::Core::Transform transform
+            (
+                startX + col * (size + spacing),
+                startY + row * (size + spacing),
+                1,
+                size,
+                size
+            );
+
+            auto sprite = m_spriteFactory.CreateButtonSprite(colors[i], transform);
+
+            m_buttons.emplace_back(std::make_unique<PixelPad::Infrastructure::UIButton<PixelPad::Core::Color>>(
+                transform,
+                std::move(sprite),
+                eventBus,
+                colors[i]
+            ));
+        }
+    }
+
+    std::vector<PixelPad::Core::Color> UIColorPickerPanel::GetColors()
+    {
+        return std::vector<PixelPad::Core::Color>
+        {
             // First row
             PixelPad::Core::Color(0, 0, 0),       // Black
             PixelPad::Core::Color(128, 128, 128), // Dark Gray
@@ -43,44 +136,5 @@ namespace PixelPad::Infrastructure
             PixelPad::Core::Color(0, 255, 128),   // Bright Green
             PixelPad::Core::Color(0, 128, 128),   // Teal
         };
-
-        int startX = 40;
-        int startY = 545;
-        int size = 13;
-        int spacing = 3;
-        int rows = 2;      
-        int cols = (colors.size() + rows - 1) / rows;
-
-        for (size_t i = 0; i < colors.size(); ++i)
-        {
-            int row = i / cols;
-            int col = i % cols;
-
-            PixelPad::Core::Transform transform
-            (
-                startX + col * (size + spacing),
-                startY + row * (size + spacing),
-                1,
-                size,
-                size
-            );
-
-            auto sprite = m_spriteFactory.CreateButtonSprite(colors[i], transform);
-
-            m_buttons.emplace_back(std::make_unique<PixelPad::Infrastructure::UIButton<PixelPad::Core::Color>>(
-                transform,
-                std::move(sprite),
-                eventBus,
-                colors[i]
-            ));
-        }
     }
-
-	void UIColorPickerPanel::Render(PixelPad::Application::IRenderer& renderer)
-	{
-		for (auto& button : m_buttons)
-		{
-			button->Render(renderer);
-		}
-	}
 }
